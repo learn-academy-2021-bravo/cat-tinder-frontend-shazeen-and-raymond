41a41a2e2b449cb002e9507606b4dad93cc7e810
Raymond Li <rlii0303@gmail.com>
2021-06-22 11:28:11 -0700

    added react routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+
+//Pages routes
 import Header from './components/Header'
 import Footer from './components/Footer'
+import CatEdit from "./pages/CatEdit";
+import CatIndex from "./pages/CatIndex";
+import CatNew from "./pages/CatNew";
+import CatShow from "./pages/CatShow";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
+//Mock Cat data
 import cats from "./mockCats"
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Header/>
-        <Footer/>
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path = "/" component={Home} />
+            <Route path = "/catindex" component={CatIndex} />  
+            <Route path = "/catshow/:id" component={CatShow}/>  
+            <Route path = "catnew" component={CatNew} />  
+            <Route path = "catedit/:id" component={CatEdit} />  
+            <Route component={NotFound} />
+          </Switch>
+          <Footer />
+        </Router>
       </div>
     )
   }
