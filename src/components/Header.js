f52ca70270ac94f17af093cf00b5b96d74434b9b
Shazeen15 <shazzy.sterlin@gmail.com>
2021-06-22 13:31:18 -0700

    edit the header file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,22 @@
-import React, { useState } from 'react'
-import {
-  
-    Navbar,
-  
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-  } from 'reactstrap';
-  
-  const Header = (props) => {
-   
-    return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">Cat Tinder</NavbarBrand>
-            <Nav className="mr-auto" navbar>
-              <NavItem>
-                <NavLink href="/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/catindex">All Cats</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/catindex">All Cats</NavLink>
-              </NavItem>
-            </Nav>
-        </Navbar>
-      </div>
-    );
-  }
-  
+import React from "react";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+
+const Header = (props) => {
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Cat Tinder</NavbarBrand>
+        <Nav className="mr-auto" navbar>
+          <NavItem>
+            <NavLink href="/">Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/catindex">All Cats</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </div>
+  );
+};
+
 export default Header;
