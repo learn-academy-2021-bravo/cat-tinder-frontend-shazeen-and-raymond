eec348bc331bdda9ccc6135a1945e7fa88c815c1
Shazeen15 <shazzy.sterlin@gmail.com>
2021-06-22 15:20:21 -0700

    fixed styling issues in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,33 @@
-import React from "react";
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+import React, { useState } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarToggler,
+  Collapse,
+} from "reactstrap";
 
 const Header = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
   return (
     <div>
       <Navbar color="light" light expand="md">
         <NavbarBrand href="/">Cat Tinder</NavbarBrand>
-        <Nav className="mr-auto" navbar>
-          <NavItem>
-            <NavLink href="/">Home</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="/catindex">All Cats</NavLink>
-          </NavItem>
-        </Nav>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/catindex">All Cats</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
       </Navbar>
     </div>
   );
