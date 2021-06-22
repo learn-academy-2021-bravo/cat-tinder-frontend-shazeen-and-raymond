28e3a49495a2d5c140364e72eef8148f479fdf77
Raymond Li <rlii0303@gmail.com>
2021-06-22 11:17:21 -0700

    added cats data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
+import cats from "./mockCats"
+
 export default class App extends Component {
   render() {
     return (
