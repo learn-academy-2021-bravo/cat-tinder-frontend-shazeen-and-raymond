7bf7b43ba806e549a11c3974f68448e540962aff
Raymond Li <rlii0303@gmail.com>
2021-06-22 11:42:12 -0700

    added reactstrap header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,34 @@
-import React, { Component } from 'react'
-
-export default class Header extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Header</h1>
-            </div>
-        )
-    }
-}
+import React, { useState } from 'react'
+import {
+  
+    Navbar,
+  
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+  } from 'reactstrap';
+  
+  const Header = (props) => {
+   
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Cat Tinder</NavbarBrand>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/catindex">All Cats</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/catindex">All Cats</NavLink>
+              </NavItem>
+            </Nav>
+        </Navbar>
+      </div>
+    );
+  }
+  
+export default Header;
