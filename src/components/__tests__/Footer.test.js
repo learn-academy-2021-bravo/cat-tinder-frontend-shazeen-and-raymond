3beba6d196e011c521e7eb9f67c74cc23ae9afc7
Raymond Li <rlii0303@gmail.com>
2021-06-23 09:55:08 -0700

    readded header & tested header/footer with enzyme/jest

--- src/components/__tests__/Footer.test.js
+++ src/components/__tests__/Footer.test.js
@@ -5,16 +5,16 @@ import Enzyme, { shallow } from 'enzyme'
 // Imports Adapter utilizing the latest react version into our test file so we can run a testing render on any component we may need.
 import Adapter from 'enzyme-adapter-react-16' 
 
-import App from "./App";
+import Footer from "../Footer";
 
 Enzyme.configure({ adapter: new Adapter() })
 
 
 describe("on initial load it should...", () => {
    
-    it("render Header", () => {
-        const appComponent = shallow(<App/>)
-        const header = appComponent.find('Header')
-        expect(header.length).toEqual(1);
+    it("render Footer", () => {
+        const footerComponent = shallow(<Footer/>)
+        const img = footerComponent.find('img')
+        expect(img.length).toEqual(1);
     })
 })
--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -5,16 +5,20 @@ import Enzyme, { shallow } from 'enzyme'
 // Imports Adapter utilizing the latest react version into our test file so we can run a testing render on any component we may need.
 import Adapter from 'enzyme-adapter-react-16' 
 
-import App from "./App";
+import Header from "../Header";
 
 Enzyme.configure({ adapter: new Adapter() })
+
 
 
 describe("on initial load it should...", () => {
    
     it("render Header", () => {
-        const appComponent = shallow(<App/>)
-        const header = appComponent.find('Header')
-        expect(header.length).toEqual(1);
+        const headerComponent = shallow(<Header/>)
+        const navBar = headerComponent.find('Navbar')
+        expect(navBar.length).toEqual(1);
     })
 })
+
+
+
