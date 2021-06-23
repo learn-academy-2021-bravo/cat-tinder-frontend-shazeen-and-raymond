d8cdc2ef2f2fca8bb985c86e6f4ac03d70cb5820
Raymond Li <rlii0303@gmail.com>
2021-06-23 11:54:53 -0700

    added all display's for one cat

--- src/pages/CatShow.js
+++ src/pages/CatShow.js
@@ -5,6 +5,8 @@ export default class CatShow extends Component {
     return (
       <div className="main-containers">
         <h1>{this.props.cat.name}</h1>
+        <p>{this.props.cat.age}</p>
+        <p>{this.props.cat.enjoys}</p>
       </div>
     );
   }
