036cded7e26220fcdf379c56f387997e32abe75c
Raymond Li <rlii0303@gmail.com>
2021-06-23 13:58:03 -0700

    finished read except for conditional rendering

--- src/pages/CatShow.js
+++ src/pages/CatShow.js
@@ -4,10 +4,16 @@ export default class CatShow extends Component {
   render() {
     return (
       <div className="main-containers">
-        <h1>{this.props.cat.name}</h1>
+        <h1>Cat Info</h1>
+        {
+        this.props.cat && <>
+        <h2>{this.props.cat.name}</h2>
         <p>{this.props.cat.age}</p>
         <p>{this.props.cat.enjoys}</p>
+        </>
+       }
       </div>
+      
     );
   }
 }
