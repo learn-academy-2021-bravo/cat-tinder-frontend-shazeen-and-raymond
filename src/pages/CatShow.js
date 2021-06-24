46afb4c4d9b035f5282c335713b759eb5be130ad
Shazeen15 <shazzy.sterlin@gmail.com>
2021-06-24 10:19:45 -0700

    completed update cat

--- src/pages/CatShow.js
+++ src/pages/CatShow.js
@@ -1,19 +1,23 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import { Button } from "reactstrap";
 
 export default class CatShow extends Component {
   render() {
     return (
       <div className="main-containers">
         <h1>Cat Info</h1>
-        {
-        this.props.cat && <>
-        <h2>{this.props.cat.name}</h2>
-        <p>{this.props.cat.age}</p>
-        <p>{this.props.cat.enjoys}</p>
-        </>
-       }
+        {this.props.cat && (
+          <>
+            <h2>{this.props.cat.name}</h2>
+            <p>{this.props.cat.age}</p>
+            <p>{this.props.cat.enjoys}</p>
+          </>
+        )}
+        <NavLink to={`/catedit/${this.props.cat.id}`}>
+          <Button color="secondary">Edit Cat Profile</Button>
+        </NavLink>
       </div>
-      
     );
   }
 }
